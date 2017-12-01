1175d04e4087c334f161bcc88a6e729f4a76eec7
SimplySerenity <mcgoughboy@gmail.com>
2017-12-01 11:42:05 -0800

    added simple remote functionality + other minor changes

--- Menu.go
+++ Menu.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/nsf/termbox-go"
 	//"log"
+	"log"
 )
 
 /*
@@ -12,15 +13,17 @@ import (
 type Menu struct {
 	Items []MenuItem
 	currentlySelected int
+	inputMode bool
 }
 
 //gets you a new menu
 func NewMenu(items []MenuItem)(Menu){
-	return Menu{items, 0}
+	return Menu{items, 0, false}
 }
 
 //shift the menu cursor down
 func (menu *Menu)CursorDown(){
+	log.Println("curDown called!")
 	if menu.currentlySelected == len(menu.Items) - 1{
 		menu.currentlySelected = 0
 	} else {
@@ -47,31 +50,55 @@ func (menu *Menu)MenuSelect(){
 func (menu *Menu)DrawMenu(yOffset int){
 	w, h := termbox.Size()
 
-	chunkRadius := (h - yOffset) / 4
+
+	if !menu.inputMode{
+		chunkRadius := (h - yOffset) / 4
 
 
+		//draw the middle (selected item)
+		tbprint(w / 2 - len(menu.Items[menu.currentlySelected].text) / 2, h / 2 + yOffset / 2, termbox.ColorBlue, termbox.ColorBlack, menu.Items[menu.currentlySelected].text)
 
-	tbprint(w / 2 - len(menu.Items[menu.currentlySelected].text) / 2, h / 2 + yOffset / 2, termbox.ColorBlue, termbox.ColorBlack, menu.Items[menu.currentlySelected].text)
+		//get the lower chunk of other items
+		lowerChunk := menu.Items[max(0, menu.currentlySelected - chunkRadius) : menu.currentlySelected]
 
-	lowerChunk := menu.Items[max(0, menu.currentlySelected - chunkRadius) : menu.currentlySelected]
+		i := 0
 
-	i := 0
+		//draw them
+		for item := range reverse(lowerChunk){
+			tbprint(w / 2 - len(item.text) / 2, h / 2 + yOffset / 2 - (i + 1) * 2, termbox.ColorWhite, termbox.ColorBlack, item.text)
+			i++
+		}
 
-	for item := range reverse(lowerChunk){
-		tbprint(w / 2 - len(item.text) / 2, h / 2 + yOffset / 2 - (i + 1) * 2, termbox.ColorWhite, termbox.ColorBlack, item.text)
-		i++
-	}
+		//get the upper chunk of other items
+		upperChunk := menu.Items[menu.currentlySelected + 1 : min(menu.currentlySelected + chunkRadius, len(menu.Items))]
 
-	upperChunk := menu.Items[menu.currentlySelected + 1 : min(menu.currentlySelected + chunkRadius, len(menu.Items))]
+		//draw them
+		for i := len(upperChunk) - 1; i >= 0; i-- {
+			item := upperChunk[i]
+			tbprint(w / 2 - len(item.text) / 2, h / 2 + yOffset / 2 + (i + 1) * 2, termbox.ColorWhite, termbox.ColorBlack, item.text)
+		}
+	} else { // simple text input, needs to be better in the future TODO future me make this thing better
+		text := "Input text"
+		tbprint(w / 2 - len(text) / 2, h / 2 - 5, termbox.ColorWhite, termbox.ColorBlack, text)
+		tbprint(w / 2 - len(inputText) / 2, h / 2, termbox.ColorWhite, termbox.ColorBlack, inputText)
+		tbprint(w / 2 - len("Input: ") - len(inputText) / 2, h / 2, termbox.ColorWhite, termbox.ColorBlack, "Input: ")
 
-	for i := len(upperChunk) - 1; i >= 0; i-- {
-		item := upperChunk[i]
-		tbprint(w / 2 - len(item.text) / 2, h / 2 + yOffset / 2 + (i + 1) * 2, termbox.ColorWhite, termbox.ColorBlack, item.text)
+		for i, button := range menu.Items{
+
+			if i == menu.currentlySelected{
+				tbprint( w / 2 - len(button.text) / 2, h / 2 + 3 + i * 2, termbox.ColorBlue, termbox.ColorBlack, button.text)
+			} else {
+				tbprint( w / 2 - len(button.text) / 2, h / 2 + 3 + i * 2, termbox.ColorWhite,termbox.ColorBlack, button.text)
+			}
+
+		}
+
 	}
 
 
 }
 
+//a weird way to reverse, thanks stackoverflow
 func reverse(lst []MenuItem) chan MenuItem {
 	ret := make(chan MenuItem)
 	go func() {
@@ -83,7 +110,7 @@ func reverse(lst []MenuItem) chan MenuItem {
 	return ret
 }
 
-
+// because go lacks ternary
 func min(a, b int)int{
 	if a <= b {
 		return a
--- main.go
+++ main.go
@@ -5,9 +5,17 @@ import (
 	"github.com/mattn/go-runewidth"
 	"log"
 	"os"
+	"net/http"
+	"strconv"
 )
 
-var menu  = Menu{nil, 0}
+/*
+	Snappy!
+*/
+
+
+var menu  = Menu{nil, 0, false}
+var inputText = ""
 
 func main() {
 	err := termbox.Init()
@@ -28,6 +36,9 @@ func main() {
 	defer f.Close()
 
 	log.SetOutput(f)
+
+	http.HandleFunc("/", remoteHandler)
+	go http.ListenAndServe(":8080", nil)
 
 	mainMenu()
 	drawAll()
@@ -89,6 +100,7 @@ func tbprint(x, y int, fg, bg termbox.Attribute, msg string) {
 	}
 }
 
+//the main menu
 func mainMenu(){
 	menu = NewMenu([]MenuItem{
 		NewMenuItem("Search", func(){
@@ -100,15 +112,73 @@ func mainMenu(){
 	})
 }
 
+//the search menu
 func searchMenu(){
 	menu = NewMenu([]MenuItem{
 		NewMenuItem("T.V. show", func(){
-			log.Println("search for t.v. show")
+			tvSearchMenu()
 		}),
-		NewMenuItem("main menu", func(){
+		NewMenuItem("Back", func(){
 			mainMenu()
 		}),
 	})
+}
+
+//the t.v. search menu
+func tvSearchMenu(){
+	menu = NewMenu([]MenuItem{
+		NewMenuItem("Enter", func(){
+			//TODO API CALL AND NEW MENU HERE
+		}),
+		NewMenuItem("Back", func(){
+			searchMenu()
+		}),
+	})
+	menu.inputMode = true
+}
+
+/*
+	This is a very simple set up to allow control of Snappy from a HTTP request, it simply parses the queries and calls the respective menu functions
+	It's very likely to cause unexpected behavior if a user is inputting with something like a keyboard and the remote at the same time
+	Because the remote is asynchronously changing the menu, but it should be fine for now because that use case is unlikely (said every programmer ever)
+	//TODO likely find a smarter way to handle these race conditions
+
+	current commands:
+	down(int) move the menu down the amount e.g. ?down=1
+	up(int) move the menu up the amount e.g. ?up=1
+	select(bool) selects the current button if true e.g. ?select=true I'm not sure why this is even a bool tbh
+	text(string) sets the input text to the given string if the menu is in input mode e.g. text?=spongebob
+*/
+func remoteHandler(w http.ResponseWriter, r *http.Request){
+	v := r.URL.Query()
+
+	if v.Get("down") != ""{
+		down, err := strconv.Atoi(v.Get("down"))
+		check(err)
+		for i := 0; i < down; i++{
+			menu.CursorDown()
+		}
+	} else if v.Get("up") != ""{
+		up, err := strconv.Atoi(v.Get("down"))
+		check(err)
+		for i := 0; i > up; i++{
+			menu.CursorUp()
+		}
+	} else if v.Get("select") != ""{
+		isSelected, err := strconv.ParseBool(v.Get("select"))
+		check(err)
+		if isSelected{
+			menu.MenuSelect()
+		}
+	} else if v.Get("text") != ""{
+		text := v.Get("text")
+		if menu.inputMode{
+			inputText = text
+		}
+	}
+
+	drawAll()
+	w.WriteHeader(http.StatusOK)
 }
 
 
